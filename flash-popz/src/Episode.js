37a3cd3f3299002da385e4aa88e1f6243cfc569b
wboeker <wendyb@alcraeft.com>
2019-10-02 00:57:09 -0400

    added images

--- flash-popz/src/Episode.js
+++ flash-popz/src/Episode.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Card } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
+import './Episode.css';
 
 class Episode extends Component{
+  constructor(props){
+    super(props);
+  }
 
   render(props){
       return(
         <div className="episode">
-          <Card className="bg-dark text-white">
-            <Card.Img src="Chihayafuru_Season_1.png" alt="Card image" />
-            <Card.ImgOverlay>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural lead-in to
-                additional content. This content is a little bit longer.
-              </Card.Text>
-              <Card.Text>Last updated 3 mins ago</Card.Text>
-            </Card.ImgOverlay>
-          </Card>
+          <Image src={this.props.image} rounded fluid/>
         </div>
       );
   }
+}
+
+Episode.propTypes = {
+  image: PropTypes.string,
 }
 
 export default Episode;
--- flash-popz/src/EpisodeList.js
+++ flash-popz/src/EpisodeList.js
@@ -6,9 +6,18 @@ import Episode from './Episode';
 class EpisodeList extends Component{
 
   render(props){
+      let shows = ["deathnote.jpg", "7seeds.jpg", "aggretsuko.jpg", "asilentvoice.jpg", "attackontitan.jpg",
+    "blueexorcist.jpg", "pokemon.jpg", "codegeass.jpg", "hunterxhunter.jpg", "ouranhostclub.jpg", "bleach.jpg",
+  "vampireknight.jpg"];
       return(
         <div className="episode-list">
-          <Episode/>
+            {shows.map((show) =>
+              {
+                  return(
+                    <Episode image={show}/>
+                  );
+              }
+            )}
         </div>
       )
   }
