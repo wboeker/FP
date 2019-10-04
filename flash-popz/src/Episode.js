0bb24f638b6a62ed56564f8ddb009deb9b5f4f0b
wboeker <wendyb@alcraeft.com>
2019-10-03 22:15:37 -0400

    added vocab data

--- flash-popz/src/Episode.js
+++ flash-popz/src/Episode.js
@@ -7,12 +7,27 @@ import { Button } from 'react-bootstrap';
 class Episode extends Component{
 
   render(props){
+      let vocab = [{word: "悪い", reading: "わるい", english: "bad", sentence: "目に悪いから暗いところで本を読んではいけない。",
+      sentReading: "め　に　わるい　から　くらい　ところ　で　ほん　を　よんで　は　いけない", engSent: "Because it is bad for your eyes, do not read under dim light."},
+      {word: "勝ち", reading: "かち", english: "win", sentence: "表が出たら僕の勝ち、裏が出たら君の勝ち。",
+      sentReading: "ひょう　が　でたら　ぼく　の　かち　うら　が　でたら　きみ　の　かち", engSent: "Heads it's my win, tails it's yours."},
+      {word: "思う", reading: "おもう", english: "to think", sentence: "試験が教育を駄目にしていると思う。",
+      sentReading: "しけん　が　きょういく　を　だめ　に　している　と　おもう", engSent: "I think exams are ruining education."},
+      {word: "仲間", reading: "なかま", english: "comrade", sentence: "彼は私の仕事仲間である。",
+      sentReading: "かれ　は　しごと　なかま　である", engSent: "He is my colleague."},
+      {word: "入れる", reading: "いれる", english: "to put in", sentence: "彼は大学に入れるように一所懸命勉強した。",
+      sentReading: "かれ　は　だいがく　に　いれる　ように　いっしょけんめい　べんきょうした", engSent: "He is my colleague."},
+      {word: "今", reading: "いま", english: "now", sentence: "列車は今着いたばかりです。",
+      sentReading: "れっしゃ　は　いま　ついた　ばらり　です", engSent: "The train has just arrived here."},
+      {word: "神", reading: "かみ", english: "God", sentence: "彼女は神を信じない",
+      sentReading: "かのじょ　は　かみ　を　しんじない", engSent: "She doesn't believe in God."}];
       return(
         <div className="episode">
           <div className="carousel-container">
             <h3 className="episode-title"> Season 1 Episode 1: Rebirth </h3>
               <div className="flash-wrapper">
-                <FlashCard/>
+                <FlashCard word={"高校"} reading={"こうこう"}
+                english={"High School"} sentence={"高校生なら、月曜日から金曜日まで高校に行かなければなりません"}/>
               </div>
             <div className="bottom-row">
               <button type="button" class="btn btn-light">Previous</button>
--- flash-popz/src/Flashcard.js
+++ flash-popz/src/Flashcard.js
@@ -3,16 +3,27 @@ import PropTypes from 'prop-types';
 import './FlashCard.css';
 
 class FlashCard extends Component{
+  constructor(props) {
+    super(props);
+  }
 
   render(props){
       return(
         <div className="flash-card">
+          <h2>{this.props.reading}</h2>
           <h1>高校</h1>
           <h1>High School</h1>
           <h2>高校生なら、月曜日から金曜日まで高校に行かなければなりません。</h2>
         </div>
       )
   }
+}
+
+FlashCard.propTypes = {
+  word: PropTypes.string,
+  reading: PropTypes.string,
+  english: PropTypes.string,
+  sentence: PropTypes.string,
 }
 
 export default FlashCard;
