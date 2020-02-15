0784ca6d12d8de36a00c718361c4707db75e2a5d
Karina Chan <kjc83@georgetown.edu>
2020-02-14 21:27:35 -0500

    adding endpoint for extracting key phrases

--- nlp/sample_key_phrases_api.py
+++ nlp/sample_key_phrases_api.py
@@ -1,36 +1,11 @@
-########### Python 2.7 #############
-import httplib, urllib, base64
-
-headers = {
-    # Request headers
-    'Content-Type': 'application/json',
-    'Ocp-Apim-Subscription-Key': '{subscription key}',
-}
-
-params = urllib.urlencode({
-    # Request parameters
-    'showStats': '{boolean}',
-})
-
-try:
-    conn = httplib.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
-    conn.request("POST", "/text/analytics/v2.1/keyPhrases?%s" % params, "{body}", headers)
-    response = conn.getresponse()
-    data = response.read()
-    print(data)
-    conn.close()
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
-
-####################################
-
 ########### Python 3.2 #############
 import http.client, urllib.request, urllib.parse, urllib.error, base64
+import json
 
 headers = {
     # Request headers
     'Content-Type': 'application/json',
-    'Ocp-Apim-Subscription-Key': '{subscription key}',
+    'Ocp-Apim-Subscription-Key': '4a7bfb6d1f82406b8cdf036fedbf202b',
 }
 
 params = urllib.parse.urlencode({
@@ -38,9 +13,14 @@ params = urllib.parse.urlencode({
     'showStats': '{boolean}',
 })
 
+with open('sample.json', 'r') as myfile:
+    data=myfile.read()
+
+print(type(data))
+
 try:
-    conn = http.client.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
-    conn.request("POST", "/text/analytics/v2.1/keyPhrases?%s" % params, "{body}", headers)
+    conn = http.client.HTTPSConnection('eastus.api.cognitive.microsoft.com')
+    conn.request("POST", "https://flashpopzkeyphrases.cognitiveservices.azure.com/text/analytics/v2.1/keyPhrases?%s" % params, data, headers)
     response = conn.getresponse()
     data = response.read()
     print(data)
