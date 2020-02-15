2febe07f3a75fa72daf442cb6cf8f618065f8d18
Karina Chan <kjc83@georgetown.edu>
2020-02-15 14:51:27 -0500

    fixed translation api

--- nlp/translate_key_phrases.py
+++ nlp/translate_key_phrases.py
@@ -5,8 +5,7 @@ import json
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-
-
+import os, requests, uuid, json
 
 def prepare_document_list():
 
@@ -26,15 +25,45 @@ def prepare_document_list():
 def translations(document_list):
     # src is source language code and dest is destination language code
     # Japanese is 'ja' and english is 'en'
-    translator = Translator()
+    # key_var_name = 'TRANSLATOR_TEXT_SUBSCRIPTION_KEY'
+    # if not key_var_name in os.environ:
+    #     raise Exception('Please set/export the environment variable: {}'.format(key_var_name))
+    subscription_key = "7c10c83b68f241569541b320e3f41f6b"
+
+    # endpoint_var_name = 'https://flashpopztranslate.cognitiveservices.azure.com/sts/v1.0/issuetoken'
+    # if not endpoint_var_name in os.environ:
+    #     raise Exception('Please set/export the environment variable: {}'.format(endpoint_var_name))
+    endpoint = 'https://api.cognitive.microsofttranslator.com/'
+    # endpoint = os.environ[endpoint_var_name]
+
+    path = '/translate?api-version=3.0'
+    params = '&to=ja'
+    constructed_url = endpoint + path + params
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': subscription_key,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+
+
     for document in document_list:
-        print("Translating most relevant words in episode " + document['id'])
+        print("Translating most relevant words in cluster: " + document['id'])
         vocab_list = []
         for word in document['keyPhrases']:
-            translation_response = translator.translate(word, src='en', dest='ja')
-            translated = translation_response.text
+            # translation_response = translator.translate(word, src='en', dest='ja')
+            # translated = translation_response.text
+            body = [{
+                'text': word
+            }]
+            request = requests.post(constructed_url, headers=headers, json=body)
+            response = request.json()
+            translated = response[0]['translations'][0]['text']
+            # translated = response_body
+            print(translated)
             hiragana = hiragana_translation(translated)
-            audio_filepath = "flash-popz/src/Data/audio_files/" + word
+            audio_filepath = "flash-popz/src/Data/audio_files/" + word+".wav"
             vocab = {
               "word": translated,
               "reading": hiragana,
@@ -48,8 +77,18 @@ def translations(document_list):
         vocab_dict = {"vocab" : vocab_list}
         print(vocab_dict)
         print(json.dumps(vocab_dict))
-        # with open('vocab_phrases.json', 'w') as outfile:
-        #     outfile.write(json.dumps(vocab_dict))
+
+    with open('vocab_phrases.json', 'w') as outfile:
+        outfile.write(json.dumps(vocab_dict))
+
+
+
+    print(json.dumps(response, sort_keys=True, indent=4,
+                     ensure_ascii=False, separators=(',', ': ')))
+
+    #
+    # translator = Translator()
+    #
 
 def add_card(vocab):
     ref = db.reference()
@@ -87,7 +126,8 @@ def hiragana_translation(data):
 if __name__ == '__main__':
     cred = credentials.Certificate("./tartanhacks2020-firebase-adminsdk-8kbdj-64275d876e.json")
     default_app = firebase_admin.initialize_app(cred, {'databaseURL': 'https://tartanhacks2020.firebaseio.com/'})
-
     document_list = prepare_document_list()
+
+
     translations(document_list)
 
