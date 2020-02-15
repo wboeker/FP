754a570762714652e52332f22a219ccc6e0087e5
dyao0210 <dyao0210@users.noreply.github.com>
2020-02-15 16:09:55 -0500

    Sound

--- flash-popz/src/Episode.js
+++ flash-popz/src/Episode.js
@@ -7,8 +7,10 @@ import { DB_CONFIG } from './Config/Firebase/db_config';
 import firebase from 'firebase/app';
 import 'firebase/database';
 import { ReactComponent as SpeakerIcon } from './speaker.svg';
+import ReactAudioPlayer from "react-audio-player";
+import ExampleSound from "./Data/apartment.wav";
 
-class Episode extends Component{
+class Episode extends Component {
   constructor(props) {
     super(props);
     this.handleSentence = this.handleSentence.bind(this);
@@ -17,7 +19,7 @@ class Episode extends Component{
     this.playSound = this.playSound.bind(this);
 
     if (!firebase.apps.length) {
-       firebase.initializeApp(DB_CONFIG);
+      firebase.initializeApp(DB_CONFIG);
     }
     this.database = firebase.database().ref().child('vocab');
 
@@ -29,7 +31,7 @@ class Episode extends Component{
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     const vocab = this.state.vocab;
 
     this.database.on('child_added', snap => {
@@ -45,7 +47,7 @@ class Episode extends Component{
       })
 
       this.setState({
-        indices: this.state.vocab.map((word, index) => {return index;})
+        indices: this.state.vocab.map((word, index) => { return index; })
       })
 
       this.setState({
@@ -54,16 +56,16 @@ class Episode extends Component{
     })
   }
 
-  getRandomCard(vocab, indices){
+  getRandomCard(vocab, indices) {
     let currentVocab = vocab[indices[Math.floor(Math.random() * indices.length)]];
     return currentVocab;
   }
 
   handleSentence(event) {
-    this.setState({sentenceOpen: !this.state.sentenceOpen});
+    this.setState({ sentenceOpen: !this.state.sentenceOpen });
   }
 
-  updateVocab(){
+  updateVocab() {
     this.state.currentVocab.isInput = true;
     this.state.sentenceOpen = false;
 
@@ -72,50 +74,52 @@ class Episode extends Component{
     })
   }
 
-  renderCorrect(){
-    if(this.state.sentenceOpen&&this.state.currentVocab.isInput){
+  renderCorrect() {
+    if (this.state.sentenceOpen && this.state.currentVocab.isInput) {
       return (<div className="sentence-wrapper">
-              <h1>{this.state.currentVocab.english}</h1>
-              </div>);
+        <h1>{this.state.currentVocab.english}</h1>
+      </div>);
     }
-    else if(this.state.sentenceOpen&&!this.state.currentVocab.isInput){
+    else if (this.state.sentenceOpen && !this.state.currentVocab.isInput) {
       return (<div className="sentence-wrapper">
-              <p>{this.state.currentVocab.sentReading}</p>
-              <h2>{this.state.currentVocab.sentence}</h2>
-              <hr/>
-              <h3>{this.state.currentVocab.engSent}</h3>
-              </div>);
+        <p>{this.state.currentVocab.sentReading}</p>
+        <h2>{this.state.currentVocab.sentence}</h2>
+        <hr />
+        <h3>{this.state.currentVocab.engSent}</h3>
+      </div>);
     }
-    else{
+    else {
       return (<FlashCard key={this.state.currentVocab.word} index={this.state.currentIndex} word={this.state.currentVocab.word} reading={this.state.currentVocab.reading}
         english={this.state.currentVocab.english} sentence={this.state.currentVocab.sentence} isInput={this.state.currentVocab.isInput}
-        updateIndices={this.updateIndices}/>);
+        updateIndices={this.updateIndices} />);
     }
   }
 
   playSound() {
     // let audio = new Audio("/Data/apartment.wav");
     // audio.play();
+
   }
-        
-  render(props){
+
+  render(props) {
     let currentVocab = this.state.currentVocab;
-    let audio = new Audio("/Data/apartment.wav");
-      return(
-        <div className="episode">
-          <div className="carousel-container">
-            <h3 className="episode-title"> Season 1 Episode 1: Rebirth </h3>
-              <div className="flash-wrapper">
-                {this.renderCorrect()}
-              </div>
-              <div className="bottom-row">
-                {currentVocab.isInput ? (<Button variant="info" onClick={this.handleSentence}> {this.state.sentenceOpen ? ("Close") : ("Answer")} </Button>) : (<Button variant="info" onClick={this.handleSentence}> {this.state.sentenceOpen ? ("Close") : ("Sentence")} </Button>)}
-                <SpeakerIcon onClick={this.playSound}/>
-                <Button variant="light" onClick={this.updateVocab}>Next</Button>
-              </div>
+    let audio = "https://firebasestorage.googleapis.com/v0/b/tartanhacks2020.appspot.com/o/" + this.state.currentVocab.english + ".wav?alt=media";
+    console.log(ExampleSound);
+    return (
+      <div className="episode">
+        <div className="carousel-container">
+          <h3 className="episode-title"> Season 1 Episode 1: Rebirth </h3>
+          <div className="flash-wrapper">
+            {this.renderCorrect()}
+          </div>
+          <div className="bottom-row">
+            {currentVocab.isInput ? (<Button variant="info" onClick={this.handleSentence}> {this.state.sentenceOpen ? ("Close") : ("Answer")} </Button>) : (<Button variant="info" onClick={this.handleSentence}> {this.state.sentenceOpen ? ("Close") : ("Sentence")} </Button>)}
+            <ReactAudioPlayer src={audio} autoPlay controls />
+            <Button variant="light" onClick={this.updateVocab}>Next</Button>
           </div>
         </div>
-      )
+      </div>
+    )
   }
 }
 
